513824d95b20960df25c21c553712517a5b80ac0
OyaYuta-09 <y.oya.sys19@morijyobi.ac.jp>
2020-06-26 12:45:13 +0900

    name insert

--- index.php
+++ index.php
@@ -102,6 +102,12 @@ try {
                         </div>
                     </div>
                     <div class="form-group row">
+                        <label for="name" class="col-sm-2 col-form-label">Name</label>
+                        <div class="col-sm-10">
+                             <input type="text" class="form-control" id="name" name="name">
+                        </div>
+                    </div>
+                    <div class="form-group row">
                         <label for="age" class="col-sm-2 col-form-label">Age</label>
                         <div class="col-sm-10">
                              <input type="text" class="form-control" id="age" name="age">
